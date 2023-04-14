1ed69b26730338ae24a816b58602a184490043da
Masashi <isiutimasasi@gmail.com>
2023-04-14 11:25:04 +0900

    a

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'blog/user_list.html', {'users': users})
 
 # Create your views here.
